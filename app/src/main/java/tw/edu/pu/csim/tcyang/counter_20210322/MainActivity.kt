7056ef99d2628fdd185d193ccc8550243381c166
cai9lizhen <o1083008@gm.pu.edu.tw>
2021-03-22 15:44:39 +0800

    commit final 03222021

--- app/src/main/java/tw/edu/pu/csim/tcyang/counter_20210322/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/counter_20210322/MainActivity.kt
@@ -6,30 +6,42 @@ import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
-    var counter:Int = 0
+    var counter: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        txv.setOnClickListener(this)
+        img.setOnClickListener(this)
+        btn4.setOnClickListener(this)
     }
 
 
-
-    fun Add(v:View){
-        if (v == btn1){
+    fun Add(v: View) {
+        if (v == btn1) {
             counter++
-        }
-        else if(v==btn2){
-            counter+=2
-        }
-        else{
-            counter=0
+        } else if (v == btn2) {
+            counter += 2
+        } else {
+            counter = 0
         }
 
         txv.text = counter.toString()
     }
 
-
+    override fun onClick(v: View?) {
+        if (v == txv) {
+            counter++
+        }
+        else if(v == img){
+            counter+=2
+        }
+        else if (v == btn4) {
+            counter = (1..100).random()
+            txv.text = counter.toString()
+        }
+        txv.text = counter.toString()
+    }
 }
